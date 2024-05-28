f0ed49b0739d4e79408582be7f3ae8be29bee15e
stnaisha23 <rifaalbadi23@gmail.com>
2024-05-28 23:32:23 +0700

    setup env

---
+++ src/main/java/com/siti/orderitem/OrderitemApplication.java
@@ -0,0 +1,13 @@
+package com.siti.orderitem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderitemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderitemApplication.class, args);
+	}
+
+}
